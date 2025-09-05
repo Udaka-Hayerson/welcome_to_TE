580836f07b2674caacf25f386b79aa6db7fa0b29
Udaka-Hayerson <60106730+Udaka-Hayerson@users.noreply.github.com>
2025-09-05 16:55:20 +0300

    completed 1-st task

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,62 @@
-import { Component } from 'react';
+import React, { Component } from "react";
 
 type IUser = {
-    name: string
-    age: number
-}
+    name: string;
+    age: number;
+};
 
 type IProps = {
-    user: IUser
-}
+    user: IUser;
+};
 
 // functional component
-const FirstComponent = ({ name, age }: IUser) => (
+const FirstComponent = React.memo(({ name, age }: IUser) => (
+    <div>
+        my name is {name}, my age is {age}
+    </div>
+));
+
+export { FirstComponent };
+
+// functional component
+const Second = ({ user: { name, age } }: IProps) => (
     <div>
         my name is {name}, my age is {age}
     </div>
 );
 
-// functional component
-// Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+export const SecondComponent = React.memo(Second, (prevProps, nextProps) => {
+    return (
+        prevProps.user.name === nextProps.user.name &&
+        prevProps.user.age === nextProps.user.age
+    );
+});
 
 // class component
-class ThirdComponent extends Component<IUser> {
+export class ThirdComponent extends PureComponent<IUser> {
     render() {
         return (
             <div>
                 my name is {this.props.name}, my age is {this.props.age}
             </div>
-        )
+        );
     }
 }
 
 // class component
-class FourthComponent extends Component<IProps> {
+export class FourthComponent extends Component<IProps> {
+    shouldComponentUpdate(nextProps) {
+        return (
+            nextProps.user.name !== this.props.user.name ||
+            nextProps.user.age !== this.props.user.age
+        );
+    }
+
     render() {
         return (
             <div>
                 my name is {this.props.user.name}, my age is {this.props.user.age}
             </div>
-        )
+        );
     }
 }
